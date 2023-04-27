501752808d3fb0717e819e912fb34f0fc5bbabc0
keanugithub <98110531+keanugithub@users.noreply.github.com>
2023-04-27 21:20:35 +0800

    Update auto_update.py

--- auto_update.py
+++ auto_update.py
@@ -1,9 +1,10 @@
 import requests
 import base64
 import json
+import os
 
 GITHUB_BRANCH = "main"
-GITHUB_TOKEN = "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"
+GITHUB_TOKEN = os.getenv('PAT')
 print(GITHUB_TOKEN)
 GITHUB_REPO_URL = "https://api.github.com/repos/keanugithub/sp-filters/contents/blocklists.txt"
 MANUAL_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/manual_source"
