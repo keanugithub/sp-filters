ccc1b8188055e76b0925cf4743b7fde753944b9b
keanugithub <98110531+keanugithub@users.noreply.github.com>
2023-04-27 19:43:13 +0800

    Update auto_update.py

--- auto_update.py
+++ auto_update.py
@@ -3,7 +3,7 @@ import base64
 import json
 
 GITHUB_BRANCH = "main"
-GITHUB_TOKEN = "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"
+GITHUB_TOKEN = "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"
 GITHUB_REPO_URL = "https://api.github.com/repos/keanugithub/sp-filters/contents/blocklists.txt"
 MANUAL_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/manual_source"
 AUTO_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/auto_source"
