f500a7c28943769970f7955dd68a142ea7d727c3
keanugithub <98110531+keanugithub@users.noreply.github.com>
2023-04-27 21:13:39 +0800

    update token

--- auto_update.py
+++ auto_update.py
@@ -3,7 +3,7 @@ import base64
 import json
 
 GITHUB_BRANCH = "main"
-GITHUB_TOKEN = "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"
+GITHUB_TOKEN = "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"
 print(GITHUB_TOKEN)
 GITHUB_REPO_URL = "https://api.github.com/repos/keanugithub/sp-filters/contents/blocklists.txt"
 MANUAL_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/manual_source"
