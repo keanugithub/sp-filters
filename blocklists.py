bcace86f16c347d81808d5f5fab186952440f2ff
keanugithub <98110531+keanugithub@users.noreply.github.com>
2023-04-28 19:24:58 +0800

    Update blocklists.py

--- blocklists.py
+++ blocklists.py
@@ -6,7 +6,7 @@ import os
 # naming assignments
 GITHUB_BRANCH = "main"
 GITHUB_TOKEN = os.environ['SuperSecret']
-GITHUB_REPO_URL = "https://api.github.com/repos/keanugithub/sp-filters/contents/blocklists.txt"
+GITHUB_FILE_URL = "https://api.github.com/repos/keanugithub/sp-filters/contents/blocklists.txt"
 
 MANUAL_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/manual_source.txt"
 AUTO_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/auto_source.txt"
@@ -15,8 +15,8 @@ AUTO_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main
 manual_source = requests.get(MANUAL_SOURCE_URL).text.strip()
 auto_source = requests.get(AUTO_SOURCE_URL).text.strip()
 
-# combine sources with blocklists.txt content
-new_content = f"[----------MANUAL SOURCE----------]\n\n{manual_source}\n\n[----------AUTO SOURCE----------]\n\n{auto_source}\n{content}"
+# create new content by combining sources
+new_content = f"[----------MANUAL SOURCE----------]\n\n{manual_source}\n\n[----------AUTO SOURCE----------]\n\n{auto_source}"
 
 # get current content of blocklists.txt from GitHub
 response = requests.get(GITHUB_FILE_URL + "?ref=" + GITHUB_BRANCH, headers={"Authorization": "Token " + os.environ['SuperSecret']})
@@ -29,7 +29,8 @@ data = {
     "sha": response.json()["sha"],
     "branch": GITHUB_BRANCH
 }
-response = requests.put(GITHUB_REPO_URL, headers={"Authorization": "Token " + os.environ['SuperSecret']}, data=json.dumps(data))
+response = requests.put(GITHUB_FILE_URL, headers={"Authorization": "Token " + os.environ['SuperSecret']}, data=json.dumps(data))
+print(response.json())
 
 # create a new commit even if there are no changes
 if response.status_code == 200:
