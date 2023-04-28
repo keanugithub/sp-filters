5696e22fec28a5227e623b43eceded1946a3eb88
keanugithub <98110531+keanugithub@users.noreply.github.com>
2023-04-28 15:29:40 +0800

    Update and rename auto_blocklists.py to blocklists.py

--- blocklists.py
+++ blocklists.py
@@ -7,17 +7,17 @@ import os
 GITHUB_BRANCH = "main"
 GITHUB_TOKEN = os.environ['SuperSecret']
 GITHUB_REPO_URL = "https://api.github.com/repos/keanugithub/sp-filters/contents/blocklists.txt"
+GITHUB_FILE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/blocklists.txt"
 
 MANUAL_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/manual_source.txt"
 AUTO_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/auto_source.txt"
 
-# fetch blocklists.txt content
-#response = requests.get(GITHUB_REPO_URL + "?ref=" + GITHUB_BRANCH, headers={"Authorization": "Token " + os.environ['SuperSecret']})
-
-
 # fetch latest manual and auto sources
 manual_source = requests.get(MANUAL_SOURCE_URL).text.strip()
 auto_source = requests.get(AUTO_SOURCE_URL).text.strip()
+
+# fetch blocklists.txt content
+response = requests.get(GITHUB_FILE_URL + "?ref=" + GITHUB_BRANCH, headers={"Authorization": "Token " + os.environ['SuperSecret']})
 content = base64.b64decode(response.json()["content"]).decode()
 
 # combine sources with blocklists.txt content
@@ -27,7 +27,7 @@ new_content = f"[----------MANUAL SOURCE----------]\n\n{manual_source}\n\n[-----
 data = {
     "message": "blocklists updated by script",
     "content": base64.b64encode(new_content.encode()).decode(),
-    #"sha": response.json()["sha"],
+    "sha": response.json()["sha"],
     "branch": GITHUB_BRANCH
 }
 response = requests.put(GITHUB_REPO_URL, headers={"Authorization": "Token " + os.environ['SuperSecret']}, data=json.dumps(data))
