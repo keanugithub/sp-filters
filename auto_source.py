a02886ceae293b3f5e200cb72ca4e6140c6e8403
keanugithub <98110531+keanugithub@users.noreply.github.com>
2023-04-28 20:22:11 +0800

    Update auto_source.py

--- auto_source.py
+++ auto_source.py
@@ -3,7 +3,6 @@ import requests
 import base64
 
 # Assigning variables
-GITHUB_BRANCH = "main"
 GITHUB_API_URL = f"https://api.github.com/repos/keanugithub/sp-filters/contents/auto_source.txt"
 GITHUB_HEADERS = { "Authorization": f"token {os.environ['SuperSecret']}" }
 EXTERNAL_URL_SOURCE = "https://big.oisd.nl/dnsmasq"
@@ -20,12 +19,10 @@ external_contents = response.content.decode()
 # Encode the external contents to base64 format
 encoded_contents = base64.b64encode(external_contents.encode()).decode()
 
-# Update the file in the repository with the contents of the external URL source, only if the sha has changed
-if encoded_contents != response_data["content"]:
-    data = {
-        "message": "Update auto_source from external URL",
-        "content": encoded_contents,
-        "branch": GITHUB_BRANCH,
-        "sha": current_sha
-    }
-    response = requests.put(GITHUB_API_URL, headers=GITHUB_HEADERS, json=data)
+# Update auto_source.txt
+data = {
+    "message": "auto-source updated by script",
+    "content": encoded_contents,
+    "branch": "main",
+    "sha": current_sha }
+response = requests.put(GITHUB_API_URL, headers=GITHUB_HEADERS, json=data)
