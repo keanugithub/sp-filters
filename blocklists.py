aa8e8f639720e60823276eff87198a46066fe3a9
keanugithub <98110531+keanugithub@users.noreply.github.com>
2023-04-28 20:10:24 +0800

    Update blocklists.py

--- blocklists.py
+++ blocklists.py
@@ -6,42 +6,27 @@ import os
 # naming assignments
 GITHUB_BRANCH = "main"
 GITHUB_TOKEN = os.environ['SuperSecret']
-GITHUB_REPO_URL = "https://api.github.com/repos/keanugithub/sp-filters/contents/blocklists.txt"
+GITHUB_FILE_URL = "https://api.github.com/repos/keanugithub/sp-filters/contents/blocklists.txt"
 
 MANUAL_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/manual_source.txt"
 AUTO_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/auto_source.txt"
-
-# fetch blocklists.txt content
-response = requests.get(GITHUB_REPO_URL + "?ref=" + GITHUB_BRANCH, headers={"Authorization": "Token " + os.environ['SuperSecret']})
-content = base64.b64decode(response.json()["content"]).decode()
 
 # fetch latest manual and auto sources
 manual_source = requests.get(MANUAL_SOURCE_URL).text.strip()
 auto_source = requests.get(AUTO_SOURCE_URL).text.strip()
 
-# combine sources with blocklists.txt content
-new_content = f"[----------MANUAL SOURCE----------]\n\n{manual_source}\n\n[----------AUTO SOURCE----------]\n\n{auto_source}\n{content}"
+# create new content by combining sources
+new_content = f"[----------MANUAL SOURCE----------]\n\n{manual_source}\n\n[----------AUTO SOURCE----------]\n\n{auto_source}"
 
-# update blocklists.txt on GitHub
+# get current content of blocklists.txt from GitHub
+response = requests.get(GITHUB_FILE_URL + "?ref=" + GITHUB_BRANCH, headers={"Authorization": "Token " + os.environ['SuperSecret']})
+current_content = base64.b64decode(response.json()["content"]).decode()
+
+# update blocklists.txt
 data = {
     "message": "blocklists updated by script",
     "content": base64.b64encode(new_content.encode()).decode(),
     "sha": response.json()["sha"],
     "branch": GITHUB_BRANCH
 }
-response = requests.put(GITHUB_REPO_URL, headers={"Authorization": "Token " + os.environ['SuperSecret']}, data=json.dumps(data))
-
-# create a commit even if there are no changes
-data = {
-    "message": "there are no updates yet",
-    "parents": [response.json()["commit"]["sha"]],
-    "tree": response.json()["commit"]["tree"]["sha"]
-}
-response = requests.post("https://api.github.com/repos/keanugithub/sp-filters/git/commits", headers={"Authorization": "Token " + os.environ['SuperSecret']}, data=json.dumps(data))
-
-# update the reference to the new commit
-data = {
-    "sha": response.json()["sha"],
-    "force": True
-}
-response = requests.patch(GITHUB_REPO_URL.replace("contents", "git/refs/heads") + "/" + GITHUB_BRANCH, headers={"Authorization": "Token " + os.environ['SuperSecret']}, data=json.dumps(data))
+response = requests.put(GITHUB_FILE_URL, headers={"Authorization": "Token " + os.environ['SuperSecret']}, data=json.dumps(data))
