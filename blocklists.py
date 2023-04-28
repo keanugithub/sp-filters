abb701f1cabf37846fec4e1edd00f45f307adcb0
keanugithub <98110531+keanugithub@users.noreply.github.com>
2023-04-28 19:22:22 +0800

    Update blocklists.py

--- blocklists.py
+++ blocklists.py
@@ -11,16 +11,16 @@ GITHUB_REPO_URL = "https://api.github.com/repos/keanugithub/sp-filters/contents/
 MANUAL_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/manual_source.txt"
 AUTO_SOURCE_URL = "https://raw.githubusercontent.com/keanugithub/sp-filters/main/auto_source.txt"
 
-# fetch blocklists.txt content
-response = requests.get(GITHUB_REPO_URL + "?ref=" + GITHUB_BRANCH, headers={"Authorization": "Token " + os.environ['SuperSecret']})
-content = base64.b64decode(response.json()["content"]).decode()
-
 # fetch latest manual and auto sources
 manual_source = requests.get(MANUAL_SOURCE_URL).text.strip()
 auto_source = requests.get(AUTO_SOURCE_URL).text.strip()
 
 # combine sources with blocklists.txt content
 new_content = f"[----------MANUAL SOURCE----------]\n\n{manual_source}\n\n[----------AUTO SOURCE----------]\n\n{auto_source}\n{content}"
+
+# get current content of blocklists.txt from GitHub
+response = requests.get(GITHUB_FILE_URL + "?ref=" + GITHUB_BRANCH, headers={"Authorization": "Token " + os.environ['SuperSecret']})
+current_content = base64.b64decode(response.json()["content"]).decode()
 
 # update blocklists.txt on GitHub
 data = {
@@ -38,5 +38,5 @@ if response.status_code == 200:
         "sha": response.json()["content"]["sha"],
         "branch": GITHUB_BRANCH
     }
-    response = requests.put(GITHUB_REPO_URL, headers={"Authorization": "Token " + os.environ['SuperSecret']}, data=json.dumps(data))
-    print(response.json())
+response = requests.put(GITHUB_REPO_URL, headers={"Authorization": "Token " + os.environ['SuperSecret']}, data=json.dumps(data))
+print(response.json())
