b94d7c6f03eee24a7de7123c7d1068888751bc98
keanugithub <98110531+keanugithub@users.noreply.github.com>
2023-05-21 01:46:09 +0800

    code optimization

--- auto_source.py
+++ auto_source.py
@@ -7,14 +7,22 @@ GITHUB_API_URL = f"https://api.github.com/repos/keanugithub/sp-filters/contents/
 GITHUB_HEADERS = { "Authorization": f"token {os.environ['SuperSecret']}" }
 EXTERNAL_URL_SOURCE = "https://big.oisd.nl/dnsmasq"
 
-# Update auto_source.txt
+# Retrieve the current sha of the auto source file from GitHub
 response = requests.get(GITHUB_API_URL, headers=GITHUB_HEADERS)
-data = {
-    "message": "auto-source updated by script",
-    "content": base64.b64encode(requests.get(EXTERNAL_URL_SOURCE).content.decode().replace("server", "local").encode()).decode(),
-    "branch": "main",
-    "sha": response.json()["sha"]
-}
+response_data = response.json()
+current_sha = response_data["sha"]
 
-# Output to GitHub
-requests.put(GITHUB_API_URL, headers=GITHUB_HEADERS, json=data)
+# Download the contents of the external URL source
+response = requests.get(EXTERNAL_URL_SOURCE)
+external_contents = response.content.decode()
+external_contents = external_contents.replace("server", "local")
+
+# Encode the external contents to base64 format
+encoded_contents = base64.b64encode(external_contents.encode()).decode()
+
+# Update auto_source.txt
+data = {
+    "content": encoded_contents,
+    "branch": "main",
+    "sha": current_sha }
+response = requests.put(GITHUB_API_URL, headers=GITHUB_HEADERS, json=data)
