58277c01148d3e0cd12f4d5d5ca4195abce3fd10
keanugithub <98110531+keanugithub@users.noreply.github.com>
2023-05-21 02:27:45 +0800

    code optimization

--- auto_source.py
+++ auto_source.py
@@ -2,18 +2,18 @@ import requests
 import base64
 import os
 
-# Assigning variables
+# assigning variables
 GITHUB_API_URL = f"https://api.github.com/repos/keanugithub/sp-filters/contents/auto_source.txt"
 GITHUB_HEADERS = {"Authorization": f"token {os.environ['SuperSecret']}"}
 EXTERNAL_SOURCE = "https://big.oisd.nl/dnsmasq"
 
-# Retrieve the current sha of the auto source file from GitHub
+# retrieve the current sha of the auto source file from GitHub
 response = requests.get(GITHUB_API_URL, headers=GITHUB_HEADERS)
 
-# uownload the contents of the external URL source and decode to base64
+# download the contents of the external URL source and decode to base64
 new_contents = requests.get(EXTERNAL_SOURCE).content.decode().replace("server", "local")
 
-# update auto_source.txt and output to GitHub
+# update and encode to auto_source.txt then output to GitHub
 data = {
     "message": "automatically updated",
     "content": base64.b64encode(new_contents.encode()).decode(),
