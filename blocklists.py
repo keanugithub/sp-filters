2c5cfdfcb8871ff334ba9e67a4c509bc3e4496ad
keanugithub <98110531+keanugithub@users.noreply.github.com>
2023-05-21 01:55:26 +0800

    optimized code

--- blocklists.py
+++ blocklists.py
@@ -1,4 +1,5 @@
 import requests
+import base64
 import json
 import os
 
@@ -17,12 +18,15 @@ auto_source = requests.get(AUTO_SOURCE_URL).text.strip()
 # create new content by combining sources
 new_content = f"#[----------MANUAL SOURCE----------]\n\n{manual_source}\n\n#[----------AUTO SOURCE----------]\n\n{auto_source}"
 
-# update blocklists.txt
+# get current content of blocklists.txt and decode
+response = requests.get(GITHUB_FILE_URL + "?ref=" + GITHUB_BRANCH, headers=GITHUB_HEADERS)
+current_content = base64.b64decode(response.json()["content"]).decode()
+
+# update and encode contents to blocklists.txt then output to GitHub
 data = {
     "message": "blocklists updated by script",
-    "content": new_content,
+    "content": base64.b64encode(new_content.encode()).decode(),
+    "sha": response.json()["sha"],
     "branch": GITHUB_BRANCH
 }
-
-# output to GitHub
 requests.put(GITHUB_FILE_URL, headers=GITHUB_HEADERS, data=json.dumps(data))
